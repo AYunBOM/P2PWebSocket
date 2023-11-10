1223f9a4dbca4c6636cf5b2bbbc75b984fcb6aac
mulkong-21 <134470507+mulkong-21@users.noreply.github.com>
2023-11-10 23:59:01 +0900

    계산하는 부분 수정

--- client.py
+++ client.py
@@ -4,6 +4,9 @@ from queue import Queue
 import random
 import numpy as np
 
+global pair_check, data_row, data_col
+pair_check = []; data_row = []; data_col = [] # 짝만 맞출 리스트, data를 저장해놓을 리스트
+
 #클라이언트가 보내는 경우
 #- 서버에게 자신의 행렬 보내기
 #- 서버에게 연산 결과 보내기
@@ -43,25 +46,40 @@ def Send(client_sock, send_queue):
             
             elif type_name == 'calculating':
                 print("연산 시작")
-                pair_cal = int(pair)
-                data, rc, rc_num = etc.split("|")
-                data = list(map(int, data.split(",")))
-                pair_check = []
-                pair_check.append(pair_cal) # (수정) [[2], [해당 가로나 세로]] 로 저장하자
+                pair_cal = int(pair) # 연산할 클라이언트 번호 곱 계산 했던거
+                data, rc, rc_num = etc.split("|") # 연산할 행이나 열, 행인지 열인지, 자신이 가진 행이나 열의 번호
+                
+                data = list(map(int, data.split(","))) # ,로 구분해서 나눈 다음 리스트로 만들기
+                pair_check.append(pair_cal) # pair_cal 저장 [2]
+                
+                # 왜 row랑 col 리스트를 나눴냐면 나중에 연산하고 값을 넘겨줄 때 자신이 행인지 열인지 알 수 없어 그래서 구분했음
                 if rc == "row":
                     print("row 데이터 받음") 
-                    row = rc_num # (수정) 행번호 열번호 바뀔 것임 수정 필요
-                    cal_row = data
+                    data_row.append([pair_cal, rc_num, data]) # [2, 6, [1, 2, 3, 4, 5]] 데이터 저장
                 else:
                     print("col 데이터 받음")
-                    col = rc_num
-                    cal_col = data
+                    data_col.append([pair_cal, rc_num, data]) # [2, 6, [1, 2, 3, 4, 5]] 데이터 저장
 
                 if pair_check.count(pair_cal) == 2: # 가로 세로 행이 2개 다 들어왔으면
                     print("둘 다 받음")
+                    x, y = 0 # pop할 위치 정할 변수들
+                    for i in data_row: # 해당 pair_mul을 가지고 있는 data_row와 data_col 속 데이터 추출
+                        if i[0] == pair_cal:
+                            cal_row_dir = i[1]
+                            cal_row = i[2]
+                            data_row.pop(x)
+                        else:
+                            x += 1
+                    for j in data_col:
+                        if j[0] == pair_cal:
+                            cal_col_dir = j[1]
+                            cal_col = j[2]
+                            data_col.pop(y)
+                        else:
+                            y += 1
                     print(cal_row, cal_col)
                     result = sum(x * y for x, y in zip(cal_row, cal_col))
-                    msg = "cal_result " + str(result) + " " + str(5) + " " + str(row) + " " + str(col)
+                    msg = "cal_result " + str(result) + " " + str(5) + " " + str(cal_row_dir) + " " + str(cal_col_dir)
                     pair_check = [x for x in pair_check if x != pair_cal]
                     print("연산 완료. 연산 결과 서버에게 보냄")
                     client_sock.send(bytes(msg.encode()))
